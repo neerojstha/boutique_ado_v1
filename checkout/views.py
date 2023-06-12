54c4cf881a7dad2589c7fdebc696d214e0ad0a83
neerojstha <nirojstha@hotmail.com>
2023-06-12 15:35:16 +0000

    added checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from .forms import OrderForm
 
 
-def checlout(request):
+def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
