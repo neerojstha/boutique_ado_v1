398de979bead3d32792dbdaebdf1d3fe589f4d7a
neerojstha <nirojstha@hotmail.com>
2023-09-26 22:47:40 +0000

    added product admin- product form,views and template

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Product, Category
 
+
 class ProductForm(forms.ModelForm):
 
     class Meta:
@@ -9,8 +10,8 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        catergories = Category.objects.all()
-        friendly_names = [(c.id, c.ger_friendly_names()) for c in catergories]
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
