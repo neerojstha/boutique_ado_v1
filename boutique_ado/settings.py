d396f39cdfdd51ac05af113fe8b0e266c4b944ea
neerojstha <nirojstha@hotmail.com>
2023-10-02 14:33:23 +0000

    setting error corrected

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -122,18 +122,15 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('postgres://lhlahmel:xF4s6gjT2aCRZhVah52lHMrURq0xD_kJ@flora.db.elephantsql.com/lhlahmel'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
-DATABASES = {
-    'default': dj_database_url.parse('postgres://lhlahmel:xF4s6gjT2aCRZhVah52lHMrURq0xD_kJ@flora.db.elephantsql.com/lhlahmel')
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
